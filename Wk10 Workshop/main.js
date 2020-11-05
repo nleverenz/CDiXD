e4dc71b9ef243d9b4336e230d635415b7ce07d47
nleverenz <nlevnyc@gmail.com>
2020-11-04 22:02:36 -0500

    Week 10

---
+++ Wk10 Workshop/main.js
@@ -0,0 +1,7 @@
+
+let buttonElement = document.querySelector('#button');
+buttonElement.onclick = function(event){
+  document.getElementById("myImg").src = "image2.jpg";
+}
+
+let myImage = document.querySelector('#images')
